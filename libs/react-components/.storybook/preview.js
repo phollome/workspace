d3050fc5bebfe59994070f851424633eb93e2501
Peter Holló <hello@songsforthe.dev>
2021-03-16 01:01:54 +0100

    feat(storybook): add dark mode plugin

--- libs/react-components/.storybook/preview.js
+++ libs/react-components/.storybook/preview.js
@@ -4,3 +4,11 @@ import { addDecorator } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
 
 addDecorator(withKnobs);
+
+export const parameters = {
+  darkMode: {
+    targetClass: "body",
+    darkClass: "dark",
+    stylePreview: true,
+  },
+};
