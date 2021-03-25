101233c51111e5c4240183c7b20445eb17c48624
Peter Holló <hello@songsforthe.dev>
2021-03-25 22:14:49 +0100

    ci(netlify): skip unchanged projects

--- tools/plugins/netlify/stop-build/index.js
+++ tools/plugins/netlify/stop-build/index.js
@@ -1,5 +1,27 @@
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
 module.exports = {
-  onInit: () => {
-    console.log("Hello world from the plugin!");
+  onInit: async (props) => {
+    const { utils } = props;
+    const project = process.env.PROJECT_NAME;
+    const lastDeploy = process.env.CACHED_COMMIT_REF;
+    const projectHasChanged = await hasProjectChanged(project, lastDeploy);
+    if (!projectHasChanged) {
+      utils.build.cancelBuild(
+        `Build was cancelled because ${currentProject} was not affected by the latest changes`
+      );
+    }
   },
 };
+
+async function hasProjectChanged(project, lastDeploy) {
+  const {
+    stdout,
+  } = await exec(
+    `yarn --silent nx print-affected --base=${lastDeploy} --head=HEAD`,
+    { encoding: "utf8" }
+  );
+  const { projects } = JSON.parse(stdout);
+  return projects.includes(project);
+}
