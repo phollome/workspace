c77f0e7cf76e3c596d9be27337564c338be00da3
Peter Holló <hello@songsforthe.dev>
2021-05-13 01:36:49 +0200

    feat(stores-api): use mongodb provider in resolver functions

--- apps/stores-api/src/app/resolvers.spec.ts
+++ apps/stores-api/src/app/resolvers.spec.ts
@@ -1,36 +1,111 @@
+import { v4 as uuid } from "uuid";
 import resolvers, { AddStoresItemInput } from "./resolvers";
 import { RemoveStoresItemPayload, Unit } from "./schema";
 
+// TODO: replace "any" from database mocks
+
 test("get number of stores items", async () => {
-  const result = await resolvers.Query.totalStoresItems();
+  const countDocumentsMock = jest.fn(async () => 0);
+
+  const databaseMock: any = {
+    collection: async () => {
+      return {
+        countDocuments: countDocumentsMock,
+      };
+    },
+  };
+
+  const result = await resolvers.Query.totalStoresItems(
+    {},
+    {},
+    { database: databaseMock }
+  );
+
   expect(result).toBe(0);
+
+  expect(countDocumentsMock).toHaveBeenCalledTimes(1);
 });
 
 test("get all stores items", async () => {
-  const result = await resolvers.Query.allStoresItems();
+  const findMock = jest.fn(() => {
+    return { map: () => ({ toArray: () => [] }) };
+  });
+
+  const databaseMock: any = {
+    collection: async () => ({
+      find: findMock,
+    }),
+  };
+
+  const result = await resolvers.Query.allStoresItems(
+    {},
+    {},
+    { database: databaseMock }
+  );
+
   expect(result.length).toBe(0);
+
+  expect(findMock).toHaveBeenCalledTimes(1);
 });
 
 test("add and remove stores item", async () => {
+  let storesItems = [];
+
+  const countDocumentsMock = jest.fn(async () => {
+    return storesItems.length;
+  });
+  const insertOneMock = jest.fn(async (doc) => {
+    const { name, amount = 0, unit } = doc;
+    const id = uuid();
+    const item = {
+      id,
+      name,
+      amount,
+      unit,
+    };
+    storesItems = [...storesItems, item];
+    return { insertedId: id };
+  });
+  const findOneAndDeleteMock = jest.fn(async (query) => {
+    const storesItem = storesItems.find((item) => item.id === query._id);
+    storesItems = storesItems.filter((item) => item.id !== query._id);
+    return { value: storesItem };
+  });
+
+  const databaseMock: any = {
+    collection: async () => ({
+      countDocuments: countDocumentsMock,
+      insertOne: insertOneMock,
+      findOneAndDelete: findOneAndDeleteMock,
+    }),
+  };
+
   const input: AddStoresItemInput = {
     name: "test",
     amount: 100,
     unit: Unit.Grams,
   };
 
-  const item = await resolvers.Mutation.addStoresItem(null, {
-    input: {
-      ...input,
+  const item = await resolvers.Mutation.addStoresItem(
+    {},
+    {
+      input: {
+        ...input,
+      },
     },
-  });
+    { database: databaseMock }
+  );
 
-  expect(await resolvers.Query.totalStoresItems()).toBe(1);
+  expect(
+    await resolvers.Query.totalStoresItems({}, {}, { database: databaseMock })
+  ).toBe(1);
 
   expect(item).toMatchObject(input);
 
   const payload: RemoveStoresItemPayload = await resolvers.Mutation.removeStoresItem(
-    null,
-    { id: item.id }
+    {},
+    { id: item.id },
+    { database: databaseMock }
   );
 
   expect(payload).toMatchObject(<RemoveStoresItemPayload>{
@@ -40,5 +115,11 @@ test("add and remove stores item", async () => {
     storesItem: { ...item },
   });
 
-  expect(await resolvers.Query.totalStoresItems()).toBe(0);
+  expect(
+    await resolvers.Query.totalStoresItems({}, {}, { database: databaseMock })
+  ).toBe(0);
+
+  expect(insertOneMock).toHaveBeenCalledTimes(1);
+  expect(findOneAndDeleteMock).toHaveBeenCalledTimes(1);
+  expect(countDocumentsMock).toHaveBeenCalledTimes(3); // two times in test, one time on remove
 });
--- apps/stores-api/src/app/resolvers.ts
+++ apps/stores-api/src/app/resolvers.ts
@@ -1,6 +1,10 @@
+import { Db } from "mongodb";
 import { v4 as uuid } from "uuid";
-import { getCollection } from "./provider";
 import { RemoveStoresItemPayload, Scalars, StoresItem, Unit } from "./schema";
+
+export interface Context {
+  database: Db;
+}
 
 export interface AddStoresItemInput {
   name: string;
@@ -12,51 +16,78 @@ export interface RemoveStoresItemProps {
   id: Scalars["ID"];
 }
 
-let storesItems: StoresItem[] = [];
-
 const resolvers = {
   Query: {
-    totalStoresItems: async () => {
-      const collection = await getCollection("stores-items");
+    totalStoresItems: async (_, args, context: Context) => {
+      const { database } = context;
+
+      const collection = await database.collection("stores-items");
+
       const total = await collection.countDocuments();
       return total;
     },
-    allStoresItems: () => storesItems,
+    allStoresItems: async (
+      _,
+      args,
+      context: Context
+    ): Promise<StoresItem[]> => {
+      const { database } = context;
+
+      const collection = await database.collection("stores-items");
+
+      const items = collection
+        .find()
+        .map((item) => {
+          const { _id: id, ...otherProps } = item;
+          return { id, ...otherProps };
+        })
+        .toArray();
+      return items;
+    },
   },
   Mutation: {
-    addStoresItem: (
-      parent,
-      props: { input: AddStoresItemInput }
-    ): StoresItem => {
-      const { name, amount = 0, unit } = props.input;
-      const id = uuid();
+    addStoresItem: async (
+      _,
+      args: { input: AddStoresItemInput },
+      context: Context
+    ): Promise<StoresItem> => {
+      const { name, amount = 0, unit } = args.input;
+      const { database } = context;
+
+      const collection = await database.collection("stores-items");
+
+      const { insertedId } = await collection.insertOne({ name, amount, unit });
+
       const item = {
-        id,
+        id: insertedId,
         name,
         amount,
         unit,
       };
-      storesItems = [...storesItems, item];
       return item;
     },
-    removeStoresItem: (
-      parent,
-      props: RemoveStoresItemProps
-    ): RemoveStoresItemPayload => {
-      const totalBefore = storesItems.length;
+    removeStoresItem: async (
+      _,
+      args: RemoveStoresItemProps,
+      context: Context
+    ): Promise<RemoveStoresItemPayload> => {
+      const { database } = context;
+      const collection = await database.collection("stores-items");
+
+      const totalBefore = await collection.countDocuments();
       let removed = false;
 
-      const storesItem = storesItems.find((item) => item.id === props.id);
-      if (storesItem !== undefined) {
-        storesItems = storesItems.filter((item) => item.id !== props.id);
+      const { value } = await collection.findOneAndDelete({ _id: args.id });
+
+      if (value !== null) {
         removed = true;
       }
-      const totalAfter = storesItems.length;
+
       return {
         removed,
         totalBefore,
-        totalAfter,
-        storesItem,
+        totalAfter: totalBefore - 1,
+        storesItem: value,
       };
     },
   },
